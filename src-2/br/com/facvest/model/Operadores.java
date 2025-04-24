2c9b87b2bb629b2d5dff7bdd521b8e771d3749b8
LarissaRafaelafogaca <100051546+LarissaRafaelafogaca@users.noreply.github.com>
2025-04-23 21:43:51 -0300

    Delete src-2/br/com/facvest directory

--- src-2/br/com/facvest/model/Operadores.java
+++
@@ -1,9 +0,0 @@
-package br.com.facvest.model;
-
-public class Operadores {
-	
-	public static final String[] OPERADORES = {"+", "-", "*", "/", "=", "==",
-											   ">", "<", ">=", "<=", "!=", "++",
-											   "--", "!", "&", "|", "%", "^", 
-											   "+=", "-=", "*=", "/="};
-}
